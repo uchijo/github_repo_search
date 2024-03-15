eab0c88661d04c328dfa2bca2d31cf1e452e8e2f
Uchijo <kgtnttki.yuta@gmail.com>
2024-03-16 03:11:22 +0900

    fixed depend on generated provider error

--- lib/repository_impl/github_search_repository_impl.dart
+++ lib/repository_impl/github_search_repository_impl.dart
@@ -1,15 +1,20 @@
 import 'dart:convert';
 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_repo_search/controller/repository/github_search_repository.dart';
 import 'package:github_repo_search/model/query/query.dart';
 import 'package:github_repo_search/model/search_result/search_result.dart';
 import 'package:github_repo_search/util/get_gh_token.dart';
 import 'package:http/http.dart' as http;
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-final githubSearchRepositoryProvider = Provider<GithubSearchRepository>(
-  (ref) => GithubSearchRepositoryImpl(token: getGHToken()),
-);
+part 'github_search_repository_impl.g.dart';
+
+@riverpod
+GithubSearchRepository githubSearchRepository(
+  GithubSearchRepositoryRef ref,
+) {
+  return GithubSearchRepositoryImpl(token: getGHToken());
+}
 
 class GithubSearchRepositoryImpl implements GithubSearchRepository {
   GithubSearchRepositoryImpl({http.Client? client, required String token})
---
+++ lib/repository_impl/github_search_repository_impl.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'github_search_repository_impl.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$githubSearchRepositoryHash() =>
+    r'eda4a288999e684394abf1d41e97318ff3eb6ce5';
+
+/// See also [githubSearchRepository].
+@ProviderFor(githubSearchRepository)
+final githubSearchRepositoryProvider =
+    AutoDisposeProvider<GithubSearchRepository>.internal(
+  githubSearchRepository,
+  name: r'githubSearchRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$githubSearchRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GithubSearchRepositoryRef
+    = AutoDisposeProviderRef<GithubSearchRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
