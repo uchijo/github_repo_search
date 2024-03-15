7a41281a04b6b3ba79129dceb6e95b887f7c5dc5
Uchijo <kgtnttki.yuta@gmail.com>
2024-03-15 22:14:00 +0900

    add query_notifier

---
+++ lib/controller/query_notifier/query_notifier.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'query_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$queryNotifierHash() => r'ffaa428e6669719db2d70381307307cc80838327';
+
+/// See also [QueryNotifier].
+@ProviderFor(QueryNotifier)
+final queryNotifierProvider =
+    AutoDisposeNotifierProvider<QueryNotifier, Query>.internal(
+  QueryNotifier.new,
+  name: r'queryNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$queryNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$QueryNotifier = AutoDisposeNotifier<Query>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
