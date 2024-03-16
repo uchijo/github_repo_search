a815e3e8d38eac0fd91033570c4e3fd3dcc66def
Uchijo <kgtnttki.yuta@gmail.com>
2024-03-16 22:54:52 +0900

    MaterialApp references themeNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:github_repo_search/controller/theme_notifier/theme_notifier.dart';
 import 'package:github_repo_search/view/page/top_page.dart';
 
 void main() async {
@@ -8,18 +9,25 @@ void main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends ConsumerWidget {
+  const MyApp({
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(themeNotifierProvider);
+
     return MaterialApp(
       title: 'github search app',
       theme: ThemeData(
         useMaterial3: true,
       ),
       darkTheme: ThemeData.dark(useMaterial3: true),
-      themeMode: ThemeMode.dark,
+      themeMode: switch (theme) {
+        AsyncData(:final value) => value,
+        _ => ThemeMode.system,
+      },
       home: const TopPage(),
     );
   }
