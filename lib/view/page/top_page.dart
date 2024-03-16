586faa3acaf0290b429b800eb1dfbed939031834
Uchijo <kgtnttki.yuta@gmail.com>
2024-03-16 23:08:51 +0900

    add theme setting button

--- lib/view/page/top_page.dart
+++ lib/view/page/top_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_repo_search/view/component/repository_list.dart';
 import 'package:github_repo_search/view/component/search_bar.dart';
+import 'package:github_repo_search/view/component/theme_setting_button.dart';
 
 class TopPage extends ConsumerWidget {
   const TopPage({super.key});
@@ -11,6 +12,9 @@ class TopPage extends ConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('GitHub Repo Search'),
+        actions: const [
+          ThemeSettingButton(),
+        ],
       ),
       body: const Column(
         children: <Widget>[
